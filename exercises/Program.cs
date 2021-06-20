5c1061cd25ff39911d452d0d15ebd5beb746ea04
PARTH SARTHI <parthsarthi2003.ps@gmail.com>
2021-06-20 22:33:48 +0530

    changes

--- exercises/Program.cs
+++ exercises/Program.cs
@@ -1,33 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace exercises
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        { 
+        private static void Main(string[] args)
+        {
             // this is dummy text.
-            Console.WriteLine("enter number between 1 to 10");
-            int number = Convert.ToInt32(Console.ReadLine());
-            int value = number;
-            if (value >= 1 && value <= 10)
+            Console.WriteLine("Enter number between 1 to 10");
+            if (int.TryParse(Console.ReadLine(), out int value))
             {
-                Console.WriteLine("its valid");
+                if (value >= 1 && value <= 10)
+                {
+                    Console.WriteLine("its valid");
+                }
+                else
+                {
+                    Console.WriteLine("not valid");
+                }
             }
-        
             else
-            { Console.WriteLine("not valid"); }
+            {
+                Console.WriteLine("not valid");
+            }
 
             int a;
             int b;
-            int max;
-            int min;
-          
-            
+
             Console.WriteLine("enter number 1");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter number 2");
@@ -36,26 +35,26 @@ namespace exercises
             {
                 Console.WriteLine(" maximum number = {0}", a);
             }
-            else 
+            else
             {
                 Console.WriteLine(" maximum number = {0}", b);
             }
 
-            int w;
-            int h;
+            int width;
+            int height;
             Console.WriteLine("enter height");
-            h = Convert.ToInt32(Console.ReadLine());
+            height = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter width");
-            w = Convert.ToInt32(Console.ReadLine());
-            if (h > w)
+            width = Convert.ToInt32(Console.ReadLine());
+            if (height > width)
             { Console.WriteLine("inamge is potrait"); }
             else
-            { Console.WriteLine("image is landscape"); 
+            {
+                Console.WriteLine("image is landscape");
             }
 
             int s;
             int c;
-            int demeritpoints=0;
 
             Console.WriteLine("enter speed limit");
             s = Convert.ToInt32(Console.ReadLine());
@@ -67,11 +66,9 @@ namespace exercises
             }
             else
             {
+                int demeritpoints = 0;
                 int speeddifference = c - s;
-                for (int sd = speeddifference / 5; sd <= speeddifference; sd += 1)
-                {
-                    demeritpoints++;
-                }
+                demeritpoints = CalculateDemeritPoint(speeddifference);
                 Console.Write("demerit points:");
                 Console.WriteLine(demeritpoints);
                 if (demeritpoints >= 12)
@@ -80,18 +77,16 @@ namespace exercises
                 }
                 else
                 {
-                    Console.WriteLine("warning demeritpoints used = {0}", speeddifference / 5);
+                    Console.WriteLine("warning demeritpoints used = {0}", demeritpoints);
                 }
-                
-                
-                
-                
 
-                
-                    
-
-              
+                Console.ReadKey();
             }
+        }
+
+        private static int CalculateDemeritPoint(int speeddifference)
+        {
+            return speeddifference / 5;
         }
     }
 }
